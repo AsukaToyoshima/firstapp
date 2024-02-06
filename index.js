162a676ccefff7454b797e89c172b34aa5aea892
Asuka Toyoshima <asuka.toyoshima@di-v.co.jp>
2024-02-06 21:00:51 +0900

    投稿処理(new, create)

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 const express = require("express")
 const app = express()
+app.use(express.urlencoded({ extended: true }))
+
+const mongoose = require("mongoose")
+mongoose.connect("mongodb+srv://asukatoyoshima:techcamp@cluster0.wam4ecw.mongodb.net/blogUserDatabase?retryWrites=true&w=majority")
+  .then(()=> { console.log("Success: Connected to MongoDB") })
+  .catch((error)=>{ console.log("Failure: Unconnected to MongoDB") })
+
+const Schema = mongoose.Schema
+const PostSchema = new Schema({
+  content: String,
+  created_at: { type: Date, default: Date.now }, //タイムスタンプ(UTC)
+})
+const PostModel = mongoose.model("Post", PostSchema)
 
 // Create
-
 app.get("/posts/new", (req, res) => {
   res.sendFile(__dirname+"/views/new.html")
 })
 
-app.post("/posts", (req, res) => {
-  res.send("投稿どね");
-
+app.post("/posts", async (req, res) => {
+  try {
+    const savedDBData = await PostModel.create(req.body);
+    console.log("データ登録に成功しました");
+  } catch(error) {
+    console.log("データ登録にエラーがありました")
+  }
+  res.redirect("/posts");
 })
 
 //Read
-app.get("/", (req, res) => {
+app.get("/posts", (req, res) => {
   res.send("こんにちは");
 })
 
