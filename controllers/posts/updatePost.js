2f53e71c9fa3acb5cbfebc0a11f8a35afd2c814a
Asuka Toyoshima <asuka.toyoshima@di-v.co.jp>
2024-02-10 15:17:04 +0900

    コメント追記

--- controllers/posts/updatePost.js
+++ controllers/posts/updatePost.js
@@ -1,11 +1,12 @@
 const PostModel = require("../../models/post")
 
+// 更新処理
 module.exports = async(req, res) => {
   try {
-    await PostModel.updateOne({_id: req.params.id}, req.body);
+    await PostModel.updateOne({_id: req.params.id}, req.body); //指定の投稿をreq.bodyの値で保存
     console.log("データ更新に成功しました");
   }catch(error){
     console.log("データ更新にエラーがありました")
   }
-  res.redirect("/posts");
+  res.redirect("/posts"); //トップページへ遷移
 }
--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,11 @@ const updateGet = require('../controllers/posts/updateGet');
 const updatePost = require('../controllers/posts/updatePost');
 const deletePost = require('../controllers/posts/deletePost');
 
-router.get('/posts', allPosts);
-router.get('/posts/create', createGet);
-router.post('/posts/create', createPost);
-router.get('/posts/update/:id', updateGet);
-router.post('/posts/update/:id', updatePost);
-router.post('/posts/delete/:id', deletePost);
+router.get('/posts', allPosts);               //投稿一覧表示
+router.get('/posts/create', createGet);       //投稿画面表示
+router.post('/posts/create', createPost);     //投稿処理
+router.get('/posts/update/:id', updateGet);   //編集画面表示
+router.post('/posts/update/:id', updatePost); //編集処理
+router.post('/posts/delete/:id', deletePost); //削除処理
 
 module.exports = router;
